c17a2c942c9422792578df8f2bd0e6b6445f430c
Dhaval Dobariya <86626640+DhavalDobariya333@users.noreply.github.com>
2025-02-16 02:06:04 +0530

    Commit

--- Dynamic Time-Table Generator/Models/TimetableInputModel.cs
+++ Dynamic Time-Table Generator/Models/TimetableInputModel.cs
@@ -1,19 +1,29 @@
-﻿namespace Dynamic_Time_Table_Generator.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace Dynamic_Time_Table_Generator.Models
 {
     public class TimetableInputModel
     {
+        [Range(1, 7, ErrorMessage = "Working days must be between 1 and 7.")]
         public int WorkingDays { get; set; }
-        public int SubjectsPerDay { get; set; }
-        public int TotalSubjects { get; set; }
-        public List<SubjectHours> Subjects { get; set; }
 
-        public class SubjectHours
-        {
-            public string SubjectName { get; set; }
-            public int Hours { get; set; }
-        }
+        [Range(1, 8, ErrorMessage = "Subjects per day must be between 1 and 8.")]
+        public int SubjectsPerDay { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Total subjects must be at least 1.")]
+        public int TotalSubjects { get; set; }
+
+        public List<SubjectHours> Subjects { get; set; } = new();
 
         public int TotalHours => WorkingDays * SubjectsPerDay;
     }
 
+    public class SubjectHours
+    {
+        [Required(ErrorMessage = "Subject name is required.")]
+        public string SubjectName { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Hours must be at least 1.")]
+        public int Hours { get; set; }
+    }
 }
